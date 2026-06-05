07f2bdac198918e1ee1b7f57e6f15e4778a664bb
Snehit <153272162+Snehitr6@users.noreply.github.com>
2026-06-05 20:27:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+// Task 1
+
+let name = prompt("Enter Your Name:");
+    let age = prompt("Enter Your Age");
+    let course = prompt("Enter Your Course:");
+
+    alert("Hello " + name + ", Welcome to " + course + " Course", "Age " + age + "");
+
+    console.log("Name:", name);
+    console.log("Course:", course);
+    console.log("Age: ", age);
+
+// Task 2
+
+var company = "Stackly";
+let salary = 40000;
+const country = "India";
+
+company = "Stackly";
+salary = 30000;
+
+console.log("Company:", company);
+console.log("Salary:", salary);
+console.log("Country:", country);
+
+// Task 3
+
+let userName = prompt("Enter Your Name:");
+
+console.log("Welcome", userName);
+
+// Task 4
+
+let knowJS = confirm("Do you know JavaScript?");
+
+console.log(knowJS);
+
+// Task 5
+
+let name = prompt("Enter Student Name:");
+let age = prompt("Enter Student Age:");
+let city = prompt("Enter Student City:");
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+
+// Task 6
+
+let course = "JavaScript";
+
+alert("Welcome to " + course);
+
+console.log("Course:", course);
+
+// Task 7
+
+document.writeln("Hello Student <br>");
+document.writeln("Welcome to JavaScript <br><br>");
+
+// Task 8
+
+let favoriteFood = "MUTTON";
+let favoriteColor = "Black";
+let favoriteMovie = "Salar";
+
+console.log("Food:", favoriteFood);
+console.log("Color:", favoriteColor);
+console.log("Movie:", favoriteMovie);
+
+// Task 9
+
+let bioName = prompt("Enter Your Name:");
+let bioAge = prompt("Enter Your Age:");
+let bioPhone = prompt("Enter Your Phone Number:");
+let bioEmail = prompt("Enter Your Email:");
+
+console.log("Name:", bioName);
+console.log("Age:", bioAge);
+console.log("Phone:", bioPhone);
+console.log("Email:", bioEmail);
+
+// Task 10
+
+let challengeName = prompt("Enter Your Name:");
+let challengeCourse = prompt("Enter Your Course:");
+
+alert("Hello " + challengeName + ", Welcome to " + challengeCourse + " Course");
+
+console.log("Name:", challengeName);
+console.log("Course:", challengeCourse);
